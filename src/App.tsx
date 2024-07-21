2117de061698fafbb78de94247152eedaf3bb514
unknown <vngo5419@gmail.com>
2024-07-21 09:30:34 +0700

    Khang

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "@/modules/header";
+import Footer from "@/modules/footer";
+import Main from "@/modules/main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/constants/ui.ts
@@ -0,0 +1,71 @@
+export default {
+  nav: [
+    {
+      name: "about",
+      value: "",
+    },
+    {
+      name: "experience",
+      value: "",
+    },
+    {
+      name: "project",
+      value: "",
+    },
+    {
+      name: "contact",
+      value: "",
+    },
+  ],
+  hero: {
+    title: "Hi, I’m Khang",
+    desc: "I design and build things.",
+  },
+  about: [
+    {
+      image: "",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis. Vivamus facilisis ligula id fringilla gravida. Suspendisse ultricies sollicitudin lacus, et rutrum nunc bibendum sed. ",
+    },
+  ],
+  experience: [
+    {
+      jobTitle: "Job Title",
+      companyName: "Company Name",
+      date: "Apr 2018 - Dec 2022",
+      responsibility: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+      ],
+    },
+    {
+      jobTitle: "Job Title",
+      companyName: "Company Name",
+      date: "Apr 2018 - Dec 2022",
+      responsibility: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis vivamus facilisis ligula",
+      ],
+    },
+  ],
+  projects: [
+    {
+      image: "",
+      name: "Project Name",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis. Vivamus facilisis ligula id fringilla gravida.",
+      techStack: ["HTML", "JS", "REACT"],
+    },
+    {
+      image: "",
+      name: "Project Name",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet vestibulum felis. Vivamus facilisis ligula id fringilla gravida.",
+      techStack: ["HTML", "JS", "REACT"],
+    },
+  ],
+  contact:{
+    desc:''
+  }
+};
